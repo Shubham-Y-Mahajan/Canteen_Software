76c17fd8fd105e35094f5ac279dbf1bb312b72c8
Shubham Mahajan <shubhamy@iitbhilai.ac.in>
2023-10-14 01:48:35 +0530

    rendered window from main.py - 1st commit on shubham

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import database as d
 import window_pdf_creation as wpc
+import PySimpleGUI
+import time
+import os
+
+
+filepath="student_database.db"
+window=wpc.create_window()
+
+while True:
+
+    event,values=window.read(timeout=200)
+    window["now"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+    if (event == PySimpleGUI.WIN_CLOSED):
+        break
+
+    print(event)
+    print(values)
+    match event:
+        case "Exit":
+            break;
+        case "Add":
+            pass
+        case "End Shift":
+            pass
 
 
 
