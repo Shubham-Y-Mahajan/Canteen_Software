13d5c387b7358bf77598a62cfce96907c9a93509
Shubham Mahajan <shubhamy@iitbhilai.ac.in>
2023-10-13 22:13:45 +0530

    second commit on branch janmay2

--- window_pdf_creation.py
+++ window_pdf_creation.py
@@ -1,5 +1,6 @@
 from fpdf import FPDF # for pdf generation
 import PySimpleGUI # for window creation
+from database import get_transactions_db
 import time
 def create_window():
     PySimpleGUI.theme("BlueMono")
@@ -28,13 +29,29 @@ def create_window():
     return window
 
 def generate_pdf(rows,timestamp):
-    pass
+    pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+    pdf.add_page()
+
+    # TITLE ROW
+    pdf.set_font(family="Times", size=8, style="B")
+    pdf.set_text_color(0, 0, 0)
+    pdf.set_fill_color(148, 255, 255)  # 94FFFF(teal bluish)
+    pdf.cell(w=50, h=10, txt=f"{timestamp}", border=1)
+    pdf.ln(10)
+
+    revenue=0
+    for item in rows:
+        pdf.cell(w=60, h=5, txt=f"{item}", border=0, ln=1)
+        revenue=revenue+int(item[2])
+    pdf.cell(w=60, h=10, txt=f"Total revenue earned from add-ons = {revenue}", border=1, ln=2)
+    pdf.output(f"Reports/{timestamp}.pdf")
     
 if __name__ == "__main__":
 	window=create_window()
 	while True:
 		event,values=window.read(timeout=200)
-		
+		window["now"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 		if(event == PySimpleGUI.WIN_CLOSED):
 			break
 		print(event)
