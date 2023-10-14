8d52dd7add7f1f91ad0432693e8fc148c470bebe
Shubham Mahajan <shubhamy@iitbhilai.ac.in>
2023-10-15 01:35:59 +0530

    Value error addressed

--- main.py
+++ main.py
@@ -21,28 +21,30 @@ while True:
         case "Exit":
             break;
         case "Add":
-            id = values['id']
-            amount=int(values['amount'])
-            if (amount>=0 and id!=''):
-                data=d.get_data(id)
-                if data!=0: # index error checking [for ID field] ( see d.get_data)
-                    balance_left=d.update_data(filepath=filepath,data=data,amount=amount)
-                    if balance_left>=0:
-                        d.write_transaction_db(filepath=filepath, data=data, amount=amount)
-                        updated_transactions = d.get_transactions_db(filepath=filepath)
-                        window['transactions'].update(values=updated_transactions)
-                        window['message'].update(value=f"{data[1]} Balance left = {balance_left}")
-                        window['id'].update(value='')
-                        window['amount'].update(value='0')
+            try:
+                id = values['id']
+                amount=int(values['amount'])
+                if (amount>=0 and id!=''):
+                    data=d.get_data(id)
+                    if data!=0: # index error checking [for ID field] ( see d.get_data)
+                        balance_left=d.update_data(filepath=filepath,data=data,amount=amount)
+                        if balance_left>=0:
+                            d.write_transaction_db(filepath=filepath, data=data, amount=amount)
+                            updated_transactions = d.get_transactions_db(filepath=filepath)
+                            window['transactions'].update(values=updated_transactions)
+                            window['message'].update(value=f"{data[1]} Balance left = {balance_left}")
+                            window['id'].update(value='')
+                            window['amount'].update(value='0')
 
-                    else :
-                        window['message'].update(value="Insufficient Balance!")
-                else:
-                    PySimpleGUI.popup("Enter a valid ID", font=20)
+                        else :
+                            window['message'].update(value="Insufficient Balance!")
+                    else:
+                        PySimpleGUI.popup("Enter a valid ID", font=20)
 
-            else: # amount <0 or ID field left blank
-                PySimpleGUI.popup("Enter a valid ID and Amount", font=20)
-        
+                else: # amount <0 or ID field left blank
+                    PySimpleGUI.popup("Enter a valid ID and Amount", font=20)
+            except ValueError: #amount field anyhing other than integers
+               PySimpleGUI.popup("Enter a valid Amount", font=20)
 
         case "End Shift":
             rows=d.end_shift(filepath=filepath)
