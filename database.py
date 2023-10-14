5df8f6c85506f5435406d003e947a0adf7657c4a
ADIReclaimer117 <adityabajpai@iitbhilai.ac.in>
2023-10-14 18:35:04 +0530

    get_data added again
    This reverts commit fc9af2a6387834a4a1fbe62539faccfc9896b76c.

--- database.py
+++ database.py
@@ -3,7 +3,19 @@ import sqlite3 # for database connection
 """ Two tables to be created - 1) student information ( which will be accessed by ID number)
 							   2) transactions ( will be used to strore the transaction performed in a shift) """
 def get_data(id): # to get student data based on ID number
-	pass
+    try:
+        connection = sqlite3.connect("student_database.db")
+        cursor = connection.cursor()
+
+        cursor.execute(f"SELECT * FROM Student_Info WHERE ID='{id}'")
+        rows = cursor.fetchall() # list of tuples now one id corresponds to one student only thus only one tuple in list
+        # print(rows)  example = [(12241730, 'Shubham Mahajan', 1000)]
+        data=list(rows[0])
+        # print(data) exapmle = [12241730, 'Shubham Mahajan', 1000]
+        connection.close()
+        return data
+    except IndexError:
+        return 0
 	
 def write_transaction_db(filepath,data,amount): # to write a transaction to database
     data.pop()
