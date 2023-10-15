12fe2d3ccf5cdcdba30988c04749244accc4f288
Shubham Mahajan <shubhamy@iitbhilai.ac.in>
2023-10-15 11:27:59 +0530

    minor bug fix

--- database.py
+++ database.py
@@ -43,7 +43,7 @@ def update_data(filepath,data,amount):
     cursor = connection.cursor()
     balance_left=data[2]-amount
     if balance_left<0:
-        return 0
+        return -1
     else:
         cursor.execute(f"UPDATE Student_Info SET Balance = {balance_left} WHERE ID='{data[0]}'")
         connection.commit()
